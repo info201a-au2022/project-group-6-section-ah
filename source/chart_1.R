4a2c3ef005576e73123b037f58eb2e8c708a7cc7
slamy2 <salmay2@uw.edu>
2022-11-13 22:42:02 -0800

    LA chart

--- source/chart_1.R
+++ source/chart_1.R
@@ -1,9 +1,9 @@
 library(ggplot2)
 LA <- read.csv("../data/los_angeles_edited.csv")
 settlements <-LA$amount_awarded
-year <-as.Date(LA$calendar_year,format = "%Y") 
+year <-(LA$calendar_year) 
 
-LA_bar_data <- data.frame(LA_settlements, LA_year)
+LA_bar_data <- data.frame(settlements, year)
 ggplot (LA_bar_data, aes(year, settlements), xlab("year"), ylab("settlements"),  
         xlim=c(2010, 2020 )) +
   geom_bar(stat = "identity") +
